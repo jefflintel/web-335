3b5eda2798f6d20a3b71d2b33e9695110b1577b6
Jeff Lintel <jefflintel@gmail.com>
2020-06-21 20:04:22 -0500

    week 8 updates

---
+++ week-8/lintel_calculator.py
@@ -0,0 +1,20 @@
+# ============================================
+# ; Title:  lintel_calculator.py
+# ; Author: Professor Krasso
+# ; Date:   16 June 2020
+# ; Modified by: Jeff Lintel
+# ; Description: demonstrate calculator functions
+# ===========================================
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
---
+++ week-8/lintel_hello_world.py
@@ -0,0 +1,9 @@
+# ============================================
+# ; Title:  lintel_hello_world.py
+# ; Author: Professor Krasso
+# ; Date:   16 June 2020
+# ; Modified by: Jeff Lintel
+# ; Description: hello world in python
+# ===========================================
+
+print('Hello World')
